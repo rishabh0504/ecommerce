a84b59c24c20b4f62516b6d75418e23cb6af38a4
rishabh0504 <rishabh.tiwari0504@gmail.com>
2018-08-19 15:44:42 +0530

    [Rishabh] Folder renaming.

--- shophere-ui/src/index.js
+++ shophere-ui/src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./Modules/App";
-import "./Include/bootstrap";
+import "./Includes/bootstrap";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./Stylesheets/app.css";
 import { Provider } from "react-redux";
